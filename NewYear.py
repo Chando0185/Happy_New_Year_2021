29f1da2a67a0f009898fd59f3c55d1eaa9a22f74
Mishu Dhar Chando <53133093+Chando0185@users.noreply.github.com>
2020-12-31 13:28:54 +0600

    Add files via upload

---
+++ NewYear.py
@@ -0,0 +1,26 @@
+import os 
+import time
+from colorama import Fore, Back
+os.system('cls')
+
+filename=["file1.txt","file2.txt", "file3.txt","file4.txt", "file5.txt", "file6.txt"]
+
+def animator(filename, delay=1, repeat=10):
+	frames=[]
+	for name in filename:
+		with open(name, "r", encoding="utf-8") as f:
+			frames.append(f.readlines())
+	for i in range(repeat):
+		for frame in frames:
+			print(Fore.RED)
+			print("".join(frame))
+			# print(Back.LIGHTMAGENTA_EX)
+			time.sleep(delay)
+			os.system('cls')
+
+
+animator(filename, delay=0.5, repeat=10)
+
+animator(filename, delay=0.1, repeat=20)
+
+animator(filename, delay=2, repeat=5)
